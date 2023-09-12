0da94325bd91e36147ce76544db91453f1416945
Victoriakerubo <victoriakerubog@gmail.com>
2023-09-12 17:46:38 +0300

    commit

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,9 +1,19 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Square = require('./5-square');
 
-class Square extends Rectangle {
+class Square {
   constructor(size) {
-    super(size, size);
+    this.size = size;
+  }
+
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.size; i++) {
+      console.log(c.repeat(this.size));
+    }
   }
 }
 
