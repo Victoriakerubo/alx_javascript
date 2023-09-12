2096be436ff47a0316630814a50a5c5f67be8267
Victoriakerubo <victoriakerubog@gmail.com>
2023-09-12 18:37:41 +0300

    commit

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -5,20 +5,26 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
+// Check if both URL and file path are provided
+if (!url || !filePath) {
+  console.error("Usage: node script.js <URL> <filePath>");
+  process.exit(1);
+}
+
 // Send a GET request to the specified URL
 request(url, (error, response, body) => {
   if (error) {
-    console.error(error.message);
+    console.error(`Error requesting URL: ${error.message}`);
   } else if (response.statusCode === 200) {
     // Write the response body to the specified file
     fs.writeFile(filePath, body, 'utf-8', (writeError) => {
       if (writeError) {
-        console.error(writeError.message);
+        console.error(`Error writing to file: ${writeError.message}`);
       } else {
         console.log(`File saved as ${filePath}`);
       }
     });
   } else {
-    console.error(`Error: ${response.statusCode}`);
+    console.error(`Error: Received status code ${response.statusCode}`);
   }
 });
