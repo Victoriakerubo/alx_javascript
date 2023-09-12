9650635c11c3cb944c82a422debd82f338279b15
Victoriakerubo <victoriakerubog@gmail.com>
2023-09-12 18:11:11 +0300

    commit

---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,28 @@
+const request = require('request');
+
+// Get the API URL from the command line arguments
+const apiUrl = process.argv[2];
+
+// Character ID for Wedge Antilles
+const characterId = 18;
+
+// Send a GET request to the Star Wars API
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error.message);
+  } else if (response.statusCode === 200) {
+    const filmData = JSON.parse(body);
+    let count = 0;
+
+    // Loop through each film and count appearances of Wedge Antilles
+    filmData.results.forEach((film) => {
+      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+        count++;
+      }
+    });
+
+    console.log(count);
+  } else {
+    console.error(`Error: ${response.statusCode}`);
+  }
+});
