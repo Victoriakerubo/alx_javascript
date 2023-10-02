b4fb056f1514b13f2f727e8a5125ba0453c91d05
Victoriakerubo <victoriakerubog@gmail.com>
2023-10-02 15:29:47 +0300

    commit

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -1,4 +1,4 @@
-// Function to set cookies
+// Function to set cookies with a 10-day expiration
 function setCookies() {
     const firstnameInput = document.getElementById('firstname');
     const emailInput = document.getElementById('email');
@@ -7,36 +7,15 @@ function setCookies() {
     const firstname = firstnameInput.value;
     const email = emailInput.value;
 
-    // Set cookies with values
-    document.cookie = `firstname=${firstname}; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
-    document.cookie = `email=${email}; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
-    
+    // Calculate the expiration date (10 days from now)
+    const expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + 10);
+
+    // Set cookies with values and expiration date
+    document.cookie = `firstname=${firstname}; expires=${expirationDate.toUTCString()}; path=/`;
+    document.cookie = `email=${email}; expires=${expirationDate.toUTCString()}; path=/`;
+
     // Clear input fields
     firstnameInput.value = '';
     emailInput.value = '';
-}
-
-// Function to show cookies
-function showCookies() {
-    // Get the cookie values
-    const cookies = document.cookie.split(';');
-
-    // Create a paragraph element
-    const paragraph = document.createElement('p');
-
-    // Build the inner HTML with cookie values
-    let cookieText = 'Cookies: ';
-    for (const cookie of cookies) {
-        const [name, value] = cookie.trim().split('=');
-        cookieText += `${name}: ${value}, `;
-    }
-
-    // Remove the trailing comma and space
-    cookieText = cookieText.slice(0, -2);
-
-    // Set the inner HTML of the paragraph
-    paragraph.innerHTML = cookieText;
-
-    // Append the paragraph at the bottom of the page
-    document.getElementById('cookieDisplay').appendChild(paragraph);
 }
